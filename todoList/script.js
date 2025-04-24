13121ea81b6f59e458528512c3e21d0c942480e9
nknaleena101 <nknaleena101@gmail.com>
2025-04-24 20:30:26 +0530

    js update

--- todoList/script.js
+++ todoList/script.js
@@ -1,17 +1,18 @@
 const inputElement = document.querySelector('#new-task');
 const todoListElement = document.querySelector('.todo-list');
 
-let todoList = JSON.parse(localStorage.getItem("todoList"));
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
 
-showHtml()
+showHtml();
 
 function addTodo(){
-    const todo = inputElement.value;
+    const todo = inputElement.value.trim(); // to avoid empty task
+    if(todo === "") return;
+
     todoList.push(todo);
+    localStorage.setItem("todoList", JSON.stringify(todoList)); 
 
     inputElement.value = "";
-
-    console.log(todoList);
     showHtml();
 }
 
@@ -22,11 +23,16 @@ function showHtml(){
             <li class="todo-item">
                     <input type="checkbox">
                     <span>${todoList[i]}</span>
-                    <button class="delete" onClick="todoList.splice(${i},1); showHtml()">X</button>
+                    <button class="delete" onClick="deleteTodo(${i})">X</button>
                 </li>
         `;
         htmlCode += html;
     }
     todoListElement.innerHTML = `${htmlCode}`;
-    
+}
+
+function deleteTodo(index) {
+    todoList.splice(index, 1);
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+    showHtml();
 }
